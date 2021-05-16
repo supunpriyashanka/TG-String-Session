c70c6fa61cece7d6c0f9b86f0c8f8a8896b72566
SUPUN <79795518+supunpriyashanka@users.noreply.github.com>
2021-05-16 14:37:53 +0530

    Update genStr.py

--- genStr.py
+++ genStr.py
@@ -159,11 +159,11 @@ Must Join Channel for Bot Updates !!
     reply_markup = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton('Support Group', url='https://t.me/linux_repo'),
-                InlineKeyboardButton('Developer', url='https://t.me/AbirHasan2005')
+                InlineKeyboardButton('Support Group', url='https://t.me/NET_HACKER_BOTs_chat'),
+                InlineKeyboardButton('Developer', url='https://t.me/Supun_priyashanka')
             ],
             [
-                InlineKeyboardButton('Bots Updates Channel', url='https://t.me/Discovery_Updates'),
+                InlineKeyboardButton('Bots Updates Channel', url='https://t.me/Network_hacker_bots'),
             ]
         ]
     )
